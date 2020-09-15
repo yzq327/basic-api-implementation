86d899a32790e415ee300367b48d078c25ee8723
yzq327 <69582746+yzq327@users.noreply.github.com>
2020-09-15 22:17:28 +0800

    Update RsController.java

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
-
+@@ -8,4 +9,11 @@
 @RestController
 public class RsController {
   private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+
+  @GetMapping("/rs/list")
+  public String getRsList(){
+    return rsList.toString();
+  }
+
+
 }
